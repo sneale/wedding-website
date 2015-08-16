5c4e65b99f7383c6d71f6ca2ac0a16bed28c8864
Sam Neale <sam.neale11@gmail.com>
2015-08-16 17:26:08 -0400

    start assets, render template

--- wedding.py
+++ wedding.py
@@ -1,9 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask.ext.assets import Environment, Bundle
 app = Flask(__name__)
+
+assets = Environment(app)
+assets.url = app.static_url_path
+scss = Bundle('css/app.scss', filters='pyscss', output='all.css')
+assets.register('scss_all', scss)
 
 @app.route('/')
 def home():
-    return 'Hello World!'
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
