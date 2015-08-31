4849e97bbe4153fac59a9053628c9b59b1b13154
Sam Neale <sam.neale11@gmail.com>
2015-08-30 21:09:10 -0400

    setup urls, start rsvp

--- wedding.py
+++ wedding.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask.ext.assets import Environment, Bundle
 app = Flask(__name__)
 
@@ -10,6 +10,21 @@ assets.register('scss_all', scss)
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/info')
+def info():
+	return render_template('info.html')
+
+
+@app.route('/rsvp', methods=['GET', 'POST'])
+def rsvp():
+	error = ''
+	if request.method == "POST":
+		if 'name' in request.form:
+			if request.form.get('name') == '':
+				error = 'Name field cannot be blank'
+				return render_template('rsvp.html', error=error)
+	return render_template('rsvp.html')
 
 if __name__ == '__main__':
     app.run()
