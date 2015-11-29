8bb64c5ed21b24210a151bf69e659a7f115b4b04
Sam Neale <sam.neale11@gmail.com>
2015-11-29 15:59:39 -0500

    dropdown and more

--- wedding.py
+++ wedding.py
@@ -24,29 +24,37 @@ def info():
 def details():
 	return render_template('details.html')
 
+@app.route('/bridal')
+def bridal():
+	return render_template('bridal.html')
+
+@app.route('/accomodations')
+def accomodations():
+	return render_template('accomodations.html')
+
 @app.route('/rsvp', methods=['GET', 'POST'])
 def rsvp():
-	from models import Guest
-	error = ''
-	if request.method == "POST":
-		if request.form.get('name') == '':
-			error = 'Name field cannot be blank'
-			return render_template('rsvp.html', error=error)
-		elif request.form.get('email') == '':
-			error = "Please enter an email address"
-			return render_template('rsvp.html', error=error)
-		if 'name' in request.form:
-			if not request.form.get('attending') and not request.form.get('notAttending'):
-				error = 'Must RSVP with a resonse'
-				return render_template('rsvp.html', error=error)
-			else:
-				attending = "Yes" if request.form.get('attending') else "No"
-				email = request.form.get('email')
-				plus_one = request.form.get('plus_one')
-				comments = request.form.get('comments')
-				guest = Guest(request.form.get('name'), attending=attending, email=email, comments=comments, plus_one=plus_one)
-				db.session.add(guest)
-				db.session.commit()
+	# from models import Guest
+	# error = ''
+	# if request.method == "POST":
+	# 	if request.form.get('name') == '':
+	# 		error = 'Name field cannot be blank'
+	# 		return render_template('rsvp.html', error=error)
+	# 	elif request.form.get('email') == '':
+	# 		error = "Please enter an email address"
+	# 		return render_template('rsvp.html', error=error)
+	# 	if 'name' in request.form:
+	# 		if not request.form.get('attending') and not request.form.get('notAttending'):
+	# 			error = 'Must RSVP with a resonse'
+	# 			return render_template('rsvp.html', error=error)
+	# 		else:
+	# 			attending = "Yes" if request.form.get('attending') else "No"
+	# 			email = request.form.get('email')
+	# 			plus_one = request.form.get('plus_one')
+	# 			comments = request.form.get('comments')
+	# 			guest = Guest(request.form.get('name'), attending=attending, email=email, comments=comments, plus_one=plus_one)
+	# 			db.session.add(guest)
+	# 			db.session.commit()
 	return render_template('rsvp.html')
 
 @app.route('/guestlist')
